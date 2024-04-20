4021b671e88025273acf91a9d750a298ef548b67
Jay <sanjaysheombar@gmail.com>
2024-04-20 23:01:06 +0200

    initial commit

---
+++ src/main/kotlin/nl/jaysh/retrobob/RetrobobApplication.kt
@@ -0,0 +1,11 @@
+package nl.jaysh.retrobob
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RetrobobApplication
+
+fun main(args: Array<String>) {
+	runApplication<RetrobobApplication>(*args)
+}
---
+++ src/test/kotlin/nl/jaysh/retrobob/RetrobobApplicationTests.kt
@@ -0,0 +1,13 @@
+package nl.jaysh.retrobob
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RetrobobApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
